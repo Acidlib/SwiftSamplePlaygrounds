749eb08623fa1621258e727c9e729f3534b02167
yiling.wu <yiling.wu@e-jan.co.jp>
2022-03-22 11:25:32 +0900

    1143. Longest Common Subsequence.playground/Contents.swift

--- 1143. Longest Common Subsequence.playground/Contents.swift
+++ 1143. Longest Common Subsequence.playground/Contents.swift
@@ -1,32 +1,17 @@
 class Solution {
-    func maximumEvenSplit(_ finalSum: Int) -> [Int] {
-        guard finalSum % 2 == 0 else {return []}
-        if finalSum == 2 { return [2] }
-        var residual = finalSum
-        var result = [Int]()
-        var num = 2
-        while (residual >= num) {
-            result.append(num)
-            residual -= num
-            num += 2
-        }
-        if (residual > 0) {
-            let final = result.last! + residual
-            result.remove(at: result.count - 1)
-            result.append(final)
-        }
-        return result
+    func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+
     }
 }
 
-
+// ------------------------------------------------------------------------------
 
 let solution = Solution()
-var answer = solution.maximumEvenSplit(12)
-print(answer == [2,4,6])
+var answer = solution.longestCommonSubsequence("abcde", "ace")
+print(answer == 3)
 
-answer = solution.maximumEvenSplit(7)
-print(answer == [])
+answer = solution.longestCommonSubsequence("abc", "abc")
+print(answer == 3)
 
-answer = solution.maximumEvenSplit(28)
-print(answer == [2,4,6,16])
+answer = solution.longestCommonSubsequence("abc", "def")
+print(answer == 0)
