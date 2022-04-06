51bd96c163db290df8847cd7f9cb5bcc252b6f59
yiling.wu <yiling.wu@e-jan.co.jp>
2022-04-06 23:52:17 +0900

    55. Jump Game

--- 55. Jump Game.playground/Contents.swift
+++ 55. Jump Game.playground/Contents.swift
@@ -1,7 +1,10 @@
 //https://leetcode.com/problems/jump-game/
 //Runtime: 340 ms, faster than 96.13% of Swift online submissions for Jump Game.
 //Memory Usage: 14.7 MB, less than 22.02% of Swift online submissions for Jump Game.
-class Solution {
+//Runtime: 574 ms, faster than 45.07% of Swift online submissions for Jump Game.
+//Memory Usage: 14.3 MB, less than 71.27% of Swift online submissions for Jump Game.
+
+class Solution1 {
     func canJump(_ nums: [Int]) -> Bool {
         guard nums.count > 0 else { return true }
         var farest = nums.first!
@@ -11,6 +14,22 @@ class Solution {
             let reach = current + nums[current]
             if reach >= (nums.count-1) { return true }
             farest = max(farest, reach)
+            current += 1
+        }
+        return false
+    }
+}
+
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        guard nums.count > 1 else { return true }
+        var farestReach = nums.first!
+        var current = 0
+        while current <= farestReach {
+            farestReach = max(farestReach, current + nums[current])
+            if farestReach >= nums.count - 1 {
+                return true
+            }
             current += 1
         }
         return false
@@ -28,4 +47,8 @@ print(answer == false)
 
 answer = solution.canJump([0, 1])
 print(answer == false)
+
+
+answer = solution.canJump([0])
+print(answer == true)
 
