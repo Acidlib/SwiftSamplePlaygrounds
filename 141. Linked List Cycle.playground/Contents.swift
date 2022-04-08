9a6c43784ed544067908133e3229ddedad613d31
yiling.wu <yiling.wu@e-jan.co.jp>
2022-04-08 21:01:18 +0900

    141. Linked List Cycle.playground

---
+++ 141. Linked List Cycle.playground/Contents.swift
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/linked-list-cycle/submissions/
+//Runtime: 90 ms, faster than 21.65% of Swift online submissions for Linked List Cycle.
+//Memory Usage: 15.6 MB, less than 69.69% of Swift online submissions for Linked List Cycle.
+
+import CoreFoundation
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        guard let head = head, let next = head.next else { return false }
+        var slow: ListNode? = head
+        var fast: ListNode? = next
+        while slow !== fast {
+            if slow == nil || fast == nil { return false }
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        return true
+    }
+}
+
+
+
+// ------------------------------------------------------------------------------
+
