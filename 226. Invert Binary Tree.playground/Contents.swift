45e014c27e214bf80fd61bef3d7b3e82a8eac532
yiling.wu <yiling.wu@e-jan.co.jp>
2022-04-11 23:52:01 +0900

    226. Invert Binary Tree

---
+++ 226. Invert Binary Tree.playground/Contents.swift
@@ -0,0 +1,43 @@
+
+//Runtime: 4 ms, faster than 76.11% of Swift online submissions for Invert Binary Tree.
+//Memory Usage: 14.1 MB, less than 76.11% of Swift online submissions for Invert Binary Tree.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root?.left == nil && root?.right == nil { return root }
+        let copyLeft = root?.left
+        root?.left = root?.right
+        root?.right = copyLeft
+        invertTree(root?.left)
+        invertTree(root?.right)
+        return root
+    }
+}
