215ebc2e8021206600b0829cdd3d86dc45971a26
acid.lib <jane.doe>
2022-07-24 11:55:39 +0900

    189. Rotate Array

--- 189. Rotate Array.playground/Contents.swift
+++ 189. Rotate Array.playground/Contents.swift
@@ -1,31 +1,27 @@
-//https://leetcode.com/problems/rotate-array/
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 //510
-//Runtime: 1032 ms, faster than 27.84% of Swift online submissions for Rotate Array.
-//Memory Usage: 21.4 MB, less than 60.52% of Swift online submissions for Rotate Array.
+//Runtime: 94 ms, faster than 60.98% of Swift online submissions for Two Sum II - Input Array Is Sorted.
+//Memory Usage: 14.5 MB, less than 90.10% of Swift online submissions for Two Sum II - Input Array Is Sorted.
 class Solution {
-    func rotate(_ nums: inout [Int], _ k: Int) {
-        var count = k
-        while count > 0 {
-            let num = nums.popLast()!
-            nums.insert(num, at: 0)
-            count -= 1
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var wishList = [Int:Int]()
+        for (index, num) in numbers.enumerated() {
+            let wish = target - num
+            if let pairIndex = wishList[num] {
+                return [pairIndex + 1, index + 1]
+            } else {
+                wishList[wish] = index
+            }
         }
+        return []
     }
 }
 
-//510
-//WRONG, because k can be bigger than nums.count
-//class Solution2 {
-//    func rotate(_ nums: inout [Int], _ k: Int) {
-//         nums = Array(nums[(nums.count - k)..<nums.count]) + Array(nums[0..<nums.count-k])
-//    }
-//}
-// ======> optimize: use modulator
-//Runtime: 272 ms, faster than 79.94% of Swift online submissions for Rotate Array.
-//Memory Usage: 21.8 MB, less than 12.30% of Swift online submissions for Rotate Array.
-class Solution2 {
-    func rotate(_ nums: inout [Int], _ k: Int) {
-        let k = k % nums.count
-         nums = Array(nums[(nums.count - k)..<nums.count]) + Array(nums[0..<nums.count-k])
-    }
-}
+let case1 = Solution().twoSum([2,7,11,15], 9)
+print(case1 == [1,2])
+
+let case2 = Solution().twoSum([2,3,4], 6)
+print(case2 == [1,3])
+
+let case3 = Solution().twoSum([-1, 0], -1)
+print(case3 == [1,2])
